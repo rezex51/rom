7afeb06261b65c36a5fe3036d5c58b414c9ec3b4
Raunak Singh <raunaksingh68317@gmail.com>
2024-12-18 11:50:22 +0530

    Wip

--- a15.sh
+++ a15.sh
@@ -28,9 +28,9 @@ echo "========================================================================"
 
 rm -rf device/oneplus/larry
 rm -rf device/oneplus/sm6375-common
-rm -rf vendor/motorola/fogos
-rm -rf vendor/motorola/sm6375-common
-rm -rf kernel/motorola/sm6375
+rm -rf vendor/oneplus/larry
+rm -rf vendor/oneplus/sm6375-common
+rm -rf kernel/oneplus/sm6375
 
 
 echo "========================================================================"
@@ -43,16 +43,16 @@ echo "CLONING BASIC MUNCH RESOURCES"
 echo "========================================================================"
 
 #1. Device Tree
-git clone https://github.com/NeonWarrior478/android_device_motorola_fogos.git -b fourteen device/motorola/fogos
+git clone https://github.com/rezex51/android_device_oneplus_larry.git -b 15 device/oneplus/larry
 
 #2. Common Device Tree
-git clone https://github.com/NeonWarrior478/android_device_motorola_sm6375-common.git -b fourteen device/motorola/sm6375-common
+git clone https://github.com/rezex51/android_device_oneplus_sm6375-common.git -b 15 device/oneplus/sm6375-common
 
 #3. Vendor Tree
-git clone https://github.com/NeonWarrior478/proprietary_vendor_motorola_fogos.git -b fourteen vendor/motorola/fogos 
+git clone https://github.com/rezex51/proprietary_vendor_oneplus_larry.git -b 15 vendor/oneplus/larry
 
 #4. Common Vendor Tree
-git clone https://github.com/NeonWarrior478/proprietary_vendor_motorola_sm6375-common.git  -b fourteen vendor/motorola/sm6375-common
+git clone https://github.com/rezex51/proprietary_vendor_oneplus_sm6375-common.git -b 15 vendor/oneplus/sm6375-common
 
 #5. Kernel Tree
 git clone https://github.com/NeonWarrior478/android_kernel_motorola_sm6375.git  -b fourteen kernel/motorola/sm6375
